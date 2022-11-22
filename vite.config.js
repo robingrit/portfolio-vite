86cf8fe7b206f4e1676ec22d5a0a4176e050318f
zorpid <91528697+robingrit@users.noreply.github.com>
2022-11-22 17:29:34 +0100

    dist
    add dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vit-project-exemple/';
-  plugins: [react()]
-})
+  base: "/deploying-vit-project-exemple/",
+  plugins: [react()],
+});
